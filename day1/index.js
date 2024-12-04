566695ba7068a6dec5c769706b86e668633a1850
patrickfoulger1 <patrickfoulger1@gmail.com>
2024-12-04 20:40:01 +0000

    second gold star

--- day1/index.js
+++ day1/index.js
@@ -6,11 +6,15 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
     console.error(err);
     return;
   }
-  proccessData(data);
+  const lists = organiseLists(data);
+  console.log(getListDistance(lists))
+  console.log(getListSimilarity(lists))
+
+
 });
 
 
-function proccessData(data) {
+function organiseLists(data) {
     const allNumbers = data.match(/\d{5}/g)
     const list1 = []
     const list2 = []
@@ -22,11 +26,31 @@ function proccessData(data) {
     list1.sort((a, b) => { return a - b}) //sorted ascending
     list2.sort((a, b) => { return a - b}) //sorted ascending
 
+    return [list1, list2]
+    
+}
+
+function getListDistance(lists) {
     let total = 0
+    const list1 = lists[0]
+    const list2 = lists[1]    
     for(let i = 0; i < list1.length; i++) {
         const difference = Math.abs(list1[i] - list2[i])
         total += difference
     }
-    
-    console.log(total)
+
+    return total
+}
+
+function getListSimilarity(lists) {
+    let similarity = 0
+    const list1 = lists[0]
+    const list2 = lists[1]
+    for(let i = 0; i < list1.length; i++) {
+        const numberToCheck = list1[i]
+        const numberOfMatches = list2.filter((num) => numberToCheck === num).length
+        similarity += (numberToCheck * numberOfMatches)
+    }
+
+    return similarity
 }
