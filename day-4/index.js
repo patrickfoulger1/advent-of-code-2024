4fd45cd4ccb64185d530cef91388cae7573680c2
patrickfoulger1 <patrickfoulger1@gmail.com>
2024-12-06 21:09:19 +0000

    get 8th gold star

--- day-4/index.js
+++ day-4/index.js
@@ -1,16 +1,17 @@
 const fs = require('node:fs');
 
-fs.readFile('./word-search.txt', 'utf8', (err, data) => {
+fs.readFile('./word-search.txt', 'utf8', (err, wordsearch) => {
   if (err) {
     console.error(err);
     return;
   }
  
-  console.log(getResult(data))
+  console.log(getXmasCount(wordsearch))
+  console.log(getMasCrossCount(wordsearch))
 
 });
 
-function getResult(wordSearch) {
+function getXmasCount(wordSearch) {
     const rows = wordSearch.split("\r\n")
     const columns = []
     const diagonals = []
@@ -71,6 +72,61 @@ function getResult(wordSearch) {
     return xmasCount
   }
 
+  function getMasCrossCount(wordSearch) {
+    let masCrossCount = 0
+    const rows = wordSearch.split("\r\n")
+    const cells = []
+
+    for(const [rowIndex, row] of rows.entries()) {
+      const rowChars = [...row]
+
+      rowChars.forEach((char, charIndex) => {
+        cells.push(new Cell(charIndex, rowIndex, char))
+      })
+     
+    }
+    
+    console.log(cells)
+    for(const cell of cells) {
+      if(cell.value === "A") {
+        if(checkIfMas(cell, cells)) {
+          masCrossCount ++
+        }
+      }
+    }
+
+
+    return masCrossCount
+  }
+
+  class Cell {
+  constructor(xPos, yPos, value) {
+    this.xPos = xPos
+    this.yPos = yPos
+    this.value = value
+  }
+}
+
+  function checkIfMas(cell, allCells) {
+    const surroundingCorners = allCells.filter((thisCell) => {
+      const validYPos = [cell.yPos + 1, cell.yPos - 1]
+      const validXPos = [cell.xPos + 1, cell.xPos - 1]
+  
+
+      return validYPos.includes(thisCell.yPos) && validXPos.includes(thisCell.xPos)
+    })
+
+    
+    
+    const surroundingCornerValues = surroundingCorners.map((cell) => {return cell.value}).join("")
+    const validPatterns = [
+      "MMSS",
+      "SSMM",
+      "MSMS",
+      "SMSM",
+    ]
+    return validPatterns.includes(surroundingCornerValues)
+  }
   function countXmas(str) {
     let xmasTotal = 0
     xmasTotal += [...str.matchAll(/(?=XMAS|SAMX)/g)].length
