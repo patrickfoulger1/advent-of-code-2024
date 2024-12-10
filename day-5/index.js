287d9d5ba3faff566cf2efebe2be37310e8ec995
patrickfoulger1 <patrickfoulger1@gmail.com>
2024-12-10 22:19:20 +0000

    Get gold star 10

--- day-5/index.js
+++ day-5/index.js
@@ -5,7 +5,8 @@ const RulesTable = {
 
 }
 
-let total = 0
+let workingUpdateTotal = 0
+let fixedUpdateTotal = 0
 
 try {
   rules = fs.readFileSync('./rules.txt', 'utf8')
@@ -43,25 +44,60 @@ rules.matchAll(/([0-9][0-9])[|]([0-9][0-9])\r\n/g).forEach(matchArr => {
  for(const updateStr of updates.split("\r\n")) {
     const nodes = updateStr.split(",")
     const isUpdateValid = nodes.every((node, nodeIndex) => isNodeValid(node, Number(nodeIndex), nodes))
+
+    
     //console.log(isUpdateValid)
 
     if(isUpdateValid){
-      total += Number(nodes[Math.floor(nodes.length/ 2)])
-
-
+      workingUpdateTotal += Number(nodes[Math.floor(nodes.length/ 2)])
+    }
+    else{
+      let fixedUpdate = fixUpdate(nodes, [])
+      fixedUpdateTotal += Number(fixedUpdate[Math.floor(nodes.length/ 2)])
     }
  }
 
- console.log(total)
+ 
+ console.log("Score of updates fixed: " + fixedUpdateTotal)
+ console.log("Score of correct updates " + workingUpdateTotal)
 
  
  function isNodeValid(node, nodeIndex, nodes){
   for( const futureNode of nodes.slice(nodeIndex + 1)) {
     if(!RulesTable[node].includes(String(futureNode)))
+      
       return false
   }
 
   return true
+ }
+
+ function fixUpdate(nodes, fixedArr) {
+
+  if(nodes.length === 0) {
+    return fixedArr
+  }
+
+  for(const [index, node] of nodes.entries()) {
+   const isNodeLeftmost = nodes.every((otherNode) =>{
+      if(node === otherNode) {
+        return true
+      }else{
+        return RulesTable[otherNode].includes(String(node))
+      }
+    })
+
+    if(isNodeLeftmost) {
+      fixedArr.push(node)
+      nodes = nodes.filter((nodeToDelete) => {
+        if (nodeToDelete === node) {
+          return false
+        }
+        return true
+      })
+      return fixUpdate(nodes, fixedArr)
+    }
+  }
  }
 
    //22, 44, 33 , 45 , 76, 4 //valid
