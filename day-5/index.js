af5b329feef6028c391cf462843f52a396610c8b
patrickfoulger1 <patrickfoulger1@gmail.com>
2024-12-10 12:14:05 +0000

    get 9th gold star

--- day-5/index.js
+++ day-5/index.js
@@ -1,11 +1,16 @@
 const fs = require('node:fs');
 let rules = ""
 let updates = ""
+const RulesTable = {
+
+}
+
+let total = 0
 
 try {
   rules = fs.readFileSync('./rules.txt', 'utf8')
 } catch (err) {
-  console.error(err);
+  
 }
 
 try {
@@ -15,4 +20,49 @@ try {
     console.error(err);
   }
   
-  console.log(rules, updates)
+
+
+
+
+
+ 
+rules.matchAll(/([0-9][0-9])[|]([0-9][0-9])\r\n/g).forEach(matchArr => {
+  const leftRule = matchArr[1]
+  const rightRule = matchArr[2]
+
+  if(RulesTable[leftRule] === undefined) {
+    RulesTable[leftRule] = []
+  }
+
+  RulesTable[leftRule].push(rightRule)
+ });
+
+ 
+
+ 
+ for(const updateStr of updates.split("\r\n")) {
+    const nodes = updateStr.split(",")
+    const isUpdateValid = nodes.every((node, nodeIndex) => isNodeValid(node, Number(nodeIndex), nodes))
+    //console.log(isUpdateValid)
+
+    if(isUpdateValid){
+      total += Number(nodes[Math.floor(nodes.length/ 2)])
+
+
+    }
+ }
+
+ console.log(total)
+
+ 
+ function isNodeValid(node, nodeIndex, nodes){
+  for( const futureNode of nodes.slice(nodeIndex + 1)) {
+    if(!RulesTable[node].includes(String(futureNode)))
+      return false
+  }
+
+  return true
+ }
+
+   //22, 44, 33 , 45 , 76, 4 //valid
+   
